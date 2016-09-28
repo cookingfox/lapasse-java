d27732b2468efbdfc5835ed75863bc2c91d44934
Abel de Beer <abel@cookingfox.nl>
2016-09-28 19:36:52 +0200

    Refactors `RxLoggerHelper` class with extra tests

--- lapasse-rx/src/main/java/com/cookingfox/lapasse/impl/logging/RxLoggerHelper.java
+++ lapasse-rx/src/main/java/com/cookingfox/lapasse/impl/logging/RxLoggerHelper.java
@@ -18,6 +18,8 @@ import rx.subscriptions.Subscriptions;
 
 import java.util.Collection;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Rx helper methods for logging features.
  */
@@ -38,42 +40,25 @@ public final class RxLoggerHelper {
     /**
      * Creates an observable for command handler errors.
      *
-     * @param commandLoggerAware The subject for which to observe command handler errors.
+     * @param loggerAware The subject for which to observe command handler errors.
      * @return An observable for command handler errors.
      */
     public static Observable<CommandHandlerError> observeCommandHandlerErrors(
-            final CommandLoggerAware commandLoggerAware) {
-        return Observable.create(new Observable.OnSubscribe<CommandHandlerError>() {
+            CommandLoggerAware loggerAware) {
+        return logCommand(requireNonNull(loggerAware), new RxCommandLogger<CommandHandlerError>() {
             @Override
-            public void call(final Subscriber<? super CommandHandlerError> subscriber) {
-                // create logger
-                final CommandLogger logger = new NoopCommandLogger() {
+            public void onCommandHandlerError(final Throwable e, final Command command) {
+                subscriber.onNext(new CommandHandlerError() {
                     @Override
-                    public void onCommandHandlerError(final Throwable e, final Command command) {
-                        subscriber.onNext(new CommandHandlerError() {
-                            @Override
-                            public Command getCommand() {
-                                return command;
-                            }
-
-                            @Override
-                            public Throwable getError() {
-                                return e;
-                            }
-                        });
+                    public Command getCommand() {
+                        return command;
                     }
-                };
 
-                // add logger
-                commandLoggerAware.addCommandLogger(logger);
-
-                // remove logger on unsubscribe
-                subscriber.add(Subscriptions.create(new Action0() {
                     @Override
-                    public void call() {
-                        commandLoggerAware.removeCommandLogger(logger);
+                    public Throwable getError() {
+                        return e;
                     }
-                }));
+                });
             }
         });
     }
@@ -81,43 +66,25 @@ public final class RxLoggerHelper {
     /**
      * Creates an observable for command handler results.
      *
-     * @param commandLoggerAware The subject for which to observe command handler results.
+     * @param loggerAware The subject for which to observe command handler results.
      * @return An observable for command handler results.
      */
     public static Observable<CommandHandlerResult> observeCommandHandlerResults(
-            final CommandLoggerAware commandLoggerAware) {
-        return Observable.create(new Observable.OnSubscribe<CommandHandlerResult>() {
+            CommandLoggerAware loggerAware) {
+        return logCommand(requireNonNull(loggerAware), new RxCommandLogger<CommandHandlerResult>() {
             @Override
-            public void call(final Subscriber<? super CommandHandlerResult> subscriber) {
-                // create logger
-                final CommandLogger logger = new NoopCommandLogger() {
+            public void onCommandHandlerResult(final Command command, final Collection<Event> events) {
+                subscriber.onNext(new CommandHandlerResult() {
                     @Override
-                    public void onCommandHandlerResult(final Command command,
-                                                       final Collection<Event> events) {
-                        subscriber.onNext(new CommandHandlerResult() {
-                            @Override
-                            public Command getCommand() {
-                                return command;
-                            }
-
-                            @Override
-                            public Collection<Event> getEvents() {
-                                return events;
-                            }
-                        });
+                    public Command getCommand() {
+                        return command;
                     }
-                };
 
-                // add logger
-                commandLoggerAware.addCommandLogger(logger);
-
-                // remove logger on unsubscribe
-                subscriber.add(Subscriptions.create(new Action0() {
                     @Override
-                    public void call() {
-                        commandLoggerAware.removeCommandLogger(logger);
+                    public Collection<Event> getEvents() {
+                        return events;
                     }
-                }));
+                });
             }
         });
     }
@@ -125,43 +92,26 @@ public final class RxLoggerHelper {
     /**
      * Creates an observable for event handler errors.
      *
-     * @param eventLoggerAware The subject for which to observe event handler errors.
-     * @param <S>              The concrete type of the state object.
+     * @param loggerAware The subject for which to observe event handler errors.
+     * @param <S>         The concrete type of the state object.
      * @return An observable for event handler errors.
      */
     public static <S extends State> Observable<EventHandlerError> observeEventHandlerErrors(
-            final EventLoggerAware<S> eventLoggerAware) {
-        return Observable.create(new Observable.OnSubscribe<EventHandlerError>() {
+            EventLoggerAware<S> loggerAware) {
+        return logEvent(requireNonNull(loggerAware), new RxEventLogger<S, EventHandlerError>() {
             @Override
-            public void call(final Subscriber<? super EventHandlerError> subscriber) {
-                // create logger
-                final EventLogger<S> logger = new NoopEventLogger<S>() {
+            public void onEventHandlerError(final Throwable e, final Event event) {
+                subscriber.onNext(new EventHandlerError() {
                     @Override
-                    public void onEventHandlerError(final Throwable e, final Event event) {
-                        subscriber.onNext(new EventHandlerError() {
-                            @Override
-                            public Event getEvent() {
-                                return event;
-                            }
-
-                            @Override
-                            public Throwable getError() {
-                                return e;
-                            }
-                        });
+                    public Throwable getError() {
+                        return e;
                     }
-                };
 
-                // add logger
-                eventLoggerAware.addEventLogger(logger);
-
-                // remove logger on unsubscribe
-                subscriber.add(Subscriptions.create(new Action0() {
                     @Override
-                    public void call() {
-                        eventLoggerAware.removeEventLogger(logger);
+                    public Event getEvent() {
+                        return event;
                     }
-                }));
+                });
             }
         });
     }
@@ -169,41 +119,91 @@ public final class RxLoggerHelper {
     /**
      * Creates an observable for event handler results.
      *
-     * @param eventLoggerAware The subject for which to observe event handler results.
-     * @param <S>              The concrete type of the state object.
+     * @param loggerAware The subject for which to observe event handler results.
+     * @param <S>         The concrete type of the state object.
      * @return An observable for event handler results.
      */
     public static <S extends State> Observable<EventHandlerResult> observeEventHandlerResults(
-            final EventLoggerAware<S> eventLoggerAware) {
-        return Observable.create(new Observable.OnSubscribe<EventHandlerResult>() {
+            EventLoggerAware<S> loggerAware) {
+        return logEvent(requireNonNull(loggerAware), new RxEventLogger<S, EventHandlerResult>() {
             @Override
-            public void call(final Subscriber<? super EventHandlerResult> subscriber) {
-                // create logger
-                final EventLogger<S> logger = new NoopEventLogger<S>() {
+            public void onEventHandlerResult(final Event event, final S newState) {
+                subscriber.onNext(new EventHandlerResult<S>() {
                     @Override
-                    public void onEventHandlerResult(final Event event, final S newState) {
-                        subscriber.onNext(new EventHandlerResult() {
-                            @Override
-                            public Event getEvent() {
-                                return event;
-                            }
-
-                            @Override
-                            public State getNewState() {
-                                return newState;
-                            }
-                        });
+                    public Event getEvent() {
+                        return event;
                     }
-                };
+
+                    @Override
+                    public S getNewState() {
+                        return newState;
+                    }
+                });
+            }
+        });
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // PRIVATE METHODS
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * Creates an observable for the provided logger implementation. Adds the logger when subscribed
+     * to the observable and removes it when unsubscribed.
+     *
+     * @param loggerAware The subject for which to observe handler log calls.
+     * @param logger      The logger implementation.
+     * @param <T>         The concrete log call VO that will be emitted by the observable.
+     * @return Observable for the provided log call VO.
+     */
+    static <T> Observable<T> logCommand(final CommandLoggerAware loggerAware,
+                                        final RxCommandLogger<T> logger) {
+        return Observable.create(new Observable.OnSubscribe<T>() {
+            @Override
+            public void call(Subscriber<? super T> subscriber) {
+                // set subscriber reference for logger
+                logger.subscriber = subscriber;
 
                 // add logger
-                eventLoggerAware.addEventLogger(logger);
+                loggerAware.addCommandLogger(logger);
 
-                // remove logger on unsubscribe
+                // remove logger on subscriber unsubscribe
                 subscriber.add(Subscriptions.create(new Action0() {
                     @Override
                     public void call() {
-                        eventLoggerAware.removeEventLogger(logger);
+                        loggerAware.removeCommandLogger(logger);
+                    }
+                }));
+            }
+        });
+    }
+
+    /**
+     * * Creates an observable for the provided logger implementation. Adds the logger when subscribed
+     * to the observable and removes it when unsubscribed.
+     *
+     * @param loggerAware The subject for which to observe handler log calls.
+     * @param logger      The logger implementation.
+     * @param <S>         The concrete type of the state object.
+     * @param <T>         The concrete log call VO that will be emitted by the observable.
+     * @return Observable for the provided log call VO.
+     */
+    static <S extends State, T> Observable<T> logEvent(final EventLoggerAware<S> loggerAware,
+                                                       final RxEventLogger<S, T> logger) {
+        return Observable.create(new Observable.OnSubscribe<T>() {
+            @Override
+            public void call(Subscriber<? super T> subscriber) {
+                // set subscriber reference for logger
+                logger.subscriber = subscriber;
+
+                // add logger
+                loggerAware.addEventLogger(logger);
+
+                // remove logger on subscriber unsubscribe
+                subscriber.add(Subscriptions.create(new Action0() {
+                    @Override
+                    public void call() {
+                        loggerAware.removeEventLogger(logger);
                     }
                 }));
             }
@@ -215,9 +215,25 @@ public final class RxLoggerHelper {
     //----------------------------------------------------------------------------------------------
 
     /**
-     * No-operation implementation of {@link CommandLogger}.
+     * Helper class for usage with Rx subscriber.
+     *
+     * @param <T> The concrete type of the value this subscriber will receive.
      */
-    static class NoopCommandLogger implements CommandLogger {
+    static abstract class SubscriberAware<T> {
+
+        /**
+         * The subscriber reference.
+         */
+        Subscriber<? super T> subscriber;
+
+    }
+
+    /**
+     * No-operation implementation of {@link CommandLogger} that is {@link SubscriberAware}.
+     *
+     * @param <T> The concrete type of the value which will be received by the subscriber.
+     */
+    static class RxCommandLogger<T> extends SubscriberAware<T> implements CommandLogger {
 
         @Override
         public void onCommandHandlerError(Throwable e, Command command) {
@@ -232,11 +248,13 @@ public final class RxLoggerHelper {
     }
 
     /**
-     * No-operation implementation of {@link EventLogger}.
+     * No-operation implementation of {@link EventLogger} that is {@link SubscriberAware}.
      *
      * @param <S> The concrete type of the state object.
+     * @param <T> The concrete type of the value which will be received by the subscriber.
      */
-    static class NoopEventLogger<S extends State> implements EventLogger<S> {
+    static class RxEventLogger<S extends State, T>
+            extends SubscriberAware<T> implements EventLogger<S> {
 
         @Override
         public void onEventHandlerError(Throwable e, Event event) {
